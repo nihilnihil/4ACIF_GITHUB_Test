400c76a4338ed5f6cbe1318e2f705c68b5f15c49
Endre Beda <Endre.Beda@novaprospekt.at>
2022-05-25 17:31:42 +0200

    Add getter and setter and toString

--- src/com/company/Beda_TestKlasse.java
+++ src/com/company/Beda_TestKlasse.java
@@ -1,5 +1,20 @@
 package com.company;
 
 public class Beda_TestKlasse {
+    String str;
 
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public String toString() {
+        return "Beda_TestKlasse{" +
+                "str='" + str + '\'' +
+                '}';
+    }
 }
