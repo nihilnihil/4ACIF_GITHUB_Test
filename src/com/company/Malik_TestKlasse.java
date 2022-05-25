a09c8466bcbd121495b500d7dbaa637a1d2e0ac6
Lisa <malik.elisabeth@gmail.com>
2022-05-25 17:34:52 +0200

    Malik

---
+++ src/com/company/Malik_TestKlasse.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Malik_TestKlasse {
+    String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public String toString() {
+        return "Malik_TestKlasse{" +
+                "str='" + str + '\'' +
+                '}';
+    }
+}
