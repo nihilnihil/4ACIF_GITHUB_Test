e9390061e7b2ac7a95e03098e85c2ea1a612e231
Begaiym Bolotbekova <begaiym.bolotbekova@gmail.com>
2022-05-25 17:33:56 +0200

    added Bolotbekova's class

---
+++ src/com/company/Bolotbekova_Klasse_Test.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Bolotbekova_Klasse_Test {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    String str;
+}
